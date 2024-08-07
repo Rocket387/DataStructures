b4607527a6ba3aab75d79714dab05dba63a09905
Rochelle Charles <roccharl@amazon.co.uk>
2024-08-07 09:43:46 +0100

    Matrix row/column search

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ BinarySearch helpful link: https://www.baeldung.com/java-binary-search
 
 SingularLLNode - https://www.geeksforgeeks.org/insertion-in-linked-list/
 CircularLLNode - https://www.geeksforgeeks.org/circular-linked-list/
+
+
+Matrix data structure
+row/column search - https://www.geeksforgeeks.org/search-in-row-wise-and-column-wise-sorted-matrix/
+
---
+++ src/main/java/org/example/Matrix/rowAndColumnSearch.java
@@ -0,0 +1,36 @@
+package org.example.Matrix;
+
+/*
+Given an N X N matrix and an integer X,
+find the position of X in the matrix if it is present.
+Otherwise, print “Element not found”.
+Every row and column of the matrix is sorted in increasing order.
+ */
+public class rowAndColumnSearch {
+
+    static int search(int[][] mat, int n, int x) {
+        if (n == 0)
+            return -1;
+
+        //traverse through the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                if (mat[i][j] == x) {
+                    System.out.println("Element found at (" + i + ", " + j + ")\n");
+                    return 1;
+                }
+        }
+        System.out.println(" Element not found");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int mat [][] = {{ 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 27, 29, 37, 48 },
+                { 32, 33, 39, 50 } };
+
+        search(mat, 4, 50);
+
+    }
+}
